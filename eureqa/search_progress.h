351c4d7af63b8c484a8ea2fdabc536883d0be34e
michael.douglas.schmidt <michael.douglas.schmidt@aac19698-193d-11df-83f1-7329d1312565>
2010-06-15 03:42:51 +0000

    Added new members to search_options.

--- eureqa/search_progress.h
+++ eureqa/search_progress.h
@@ -9,75 +9,75 @@ namespace eureqa
 struct search_progress
 {
 public:
-    solution_info solution_; // a solution recently added server's solution frontier
-    float generations_; // total generations completed
-    float generations_per_sec_; // generations speed
-    float evaluations_; // total times any equation was evaluated
-    float evaluations_per_sec_; // evaluation speed
-    int total_population_size_; // total number of individuals in the current population
-    
+	solution_info solution_; // a solution recently added server's solution frontier
+	float generations_; // total generations completed
+	float generations_per_sec_; // generations speed
+	float evaluations_; // total times any equation was evaluated
+	float evaluations_per_sec_; // evaluation speed
+	int total_population_size_; // total number of individuals in the current population
+	
 public:
-    // default constructor 
-    search_progress();
-    
-    // tests if fields are entered and in range
-    bool is_valid() const;
-    
-    // returns a short text summary of the search progress
-    std::string summary() const;
-    
+	// default constructor 
+	search_progress();
+	
+	// tests if fields are entered and in range
+	bool is_valid() const;
+	
+	// returns a short text summary of the search progress
+	std::string summary() const;
+	
 protected:
-    // boost serialization, used for sending over it the network
-    // can also be used for saving/loading the data set
-    friend class boost::serialization::access;
-    template<class TArchive> void serialize(TArchive& ar, const unsigned int version);
+	// boost serialization, used for sending over it the network
+	// can also be used for saving/loading the data set
+	friend class boost::serialization::access;
+	template<class TArchive> void serialize(TArchive& ar, const unsigned int version);
 };
 
 /*---------------------------------------------------------
-    Implementation:
+	Implementation:
 *--------------------------------------------------------*/
 inline
 search_progress::search_progress() :
-    generations_(0),
-    generations_per_sec_(0),
-    evaluations_(0),
-    evaluations_per_sec_(0),
-    total_population_size_(0)
+	generations_(0),
+	generations_per_sec_(0),
+	evaluations_(0),
+	evaluations_per_sec_(0),
+	total_population_size_(0)
 { }
 
 inline
 bool search_progress::is_valid() const
 {
-    return (generations_ >= 0)
-        && (generations_per_sec_ >= 0)
-        && (evaluations_ >= 0)
-        && (evaluations_per_sec_ >= 0)
-        && (total_population_size_ >= 0)
-        ;
+	return (generations_ >= 0)
+		&& (generations_per_sec_ >= 0)
+		&& (evaluations_ >= 0)
+		&& (evaluations_per_sec_ >= 0)
+		&& (total_population_size_ >= 0)
+		;
 }
 
 inline 
 std::string search_progress::summary() const
 {
-    std::ostringstream os;
-    if (!is_valid()) { os << "Invalid! "; }
-    os << generations_ << " generations";
-    //os << " (" << generations_per_sec_ << " per sec)";
-    os << ", " << evaluations_ << " evaluations";
-    //os << " (" << evaluations_per_sec_ << " per sec)";
-    return os.str();
+	std::ostringstream os;
+	if (!is_valid()) { os << "Invalid! "; }
+	os << generations_ << " generations";
+	//os << " (" << generations_per_sec_ << " per sec)";
+	os << ", " << evaluations_ << " evaluations";
+	//os << " (" << evaluations_per_sec_ << " per sec)";
+	return os.str();
 }
 
 template<typename TArchive>
 inline
 void search_progress::serialize(TArchive& ar, const unsigned int /*version*/) 
 {
-    ar & BOOST_SERIALIZATION_NVP( solution_ );
-    ar & BOOST_SERIALIZATION_NVP( generations_ );
-    ar & BOOST_SERIALIZATION_NVP( generations_per_sec_ );
-    ar & BOOST_SERIALIZATION_NVP( evaluations_ );
-    ar & BOOST_SERIALIZATION_NVP( evaluations_per_sec_ );
-    ar & BOOST_SERIALIZATION_NVP( total_population_size_ );
+	ar & BOOST_SERIALIZATION_NVP( solution_ );
+	ar & BOOST_SERIALIZATION_NVP( generations_ );
+	ar & BOOST_SERIALIZATION_NVP( generations_per_sec_ );
+	ar & BOOST_SERIALIZATION_NVP( evaluations_ );
+	ar & BOOST_SERIALIZATION_NVP( evaluations_per_sec_ );
+	ar & BOOST_SERIALIZATION_NVP( total_population_size_ );
 }
 
 } // namespace eureqa
